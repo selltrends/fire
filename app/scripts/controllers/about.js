b6692eee5ebad3bb77727702c3d8a0829b4e9866
Alex Wang <selltrends@gmail.com>
2015-12-07 21:30:14 +0800

    init firebase project:fire

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fireApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fireApp
+ */
+angular.module('fireApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fireApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fireApp
+ */
+angular.module('fireApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
